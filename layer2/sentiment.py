c538fc0d3aab7259ef308f701ebaf49cddcdaf44
DanRenS-g-d <drengifosulbaran@gmail.com>
2026-03-04 04:55:10 +0000

    fix: update HuggingFace API URL to router

--- layer2/sentiment.py
+++ layer2/sentiment.py
@@ -12,7 +12,7 @@ import json
 from datetime import datetime, timedelta
 from db import get_connection, init_sentiment_table
 
-HF_API_URL = "https://api-inference.huggingface.co/models/ProsusAI/finbert"
+HF_API_URL = "https://router.huggingface.co/hf-inference/models/ProsusAI/finbert"
 HF_TOKEN = os.environ.get("HF_TOKEN", "")
 
 
